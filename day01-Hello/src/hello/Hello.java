393eb207d19c2768ab50b5d044c896663992b11a
GIL SU PARK <prosild@naver.com>
2018-07-04 11:54:38 +0900

    7월 4일 Java 1일차 기본형 boolean, char까지 오전 11시 54분까지

---
+++ day01-Hello/src/hello/Hello.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		/* 
+		 * 이클립스 기본설정
+		 * 
+		 * git 연결
+		 * jdk 버전 확인
+		 * workspace -> 인코딩변경
+		 * Text editors -> insert spaces for tabs 설정
+		 * 
+		 */
+		
+		// 자바 기본 출력 구문
+		// sysout -> ctrl + space키 입력 후
+		// code assistant에서 선택
+		System.out.println("Hello, World!");
+		
+		// 실행 ctrl + F11
+		
+	}
+}
