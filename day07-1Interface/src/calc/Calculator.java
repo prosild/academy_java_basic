b411f40edcd900e4ce6fc115f0c3bc97c24aa318
GIL SU PARK <prosild@naver.com>
2018-07-12 17:35:15 +0900

    7월 12일 Java 7일차 generic까지 오후 5시 35분까지

---
+++ day07-1Interface/src/calc/Calculator.java
@@ -0,0 +1,50 @@
+package calc;
+
+/**
+ * static 영역과 non-static 영역 사이의
+ * 초기화 시점에 따른 호출 방향을 테스트하는 클래스
+ * 
+ * @author PC38218
+ *
+ */
+public class Calculator {
+	
+	// add
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	// subtract
+	public int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	// multiply
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	// divide
+	public int divide(int x, int y) {
+		return x / y;
+	}
+	
+	// modular
+	public int modular(int x, int y) {
+		return x % y;
+	}
+	
+	// static 메소드 선언
+	public static double add(double x, double y) {
+		return x + y;
+	}
+	
+	public static double subtract(double x, double y) {
+		return x - y;
+	}
+	
+	public static double multiply(double x, double y) {
+		return x * y;
+	}
+	
+}
